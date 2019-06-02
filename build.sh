5ed3d29767ec83af58daab1bac2fa1d0d67ed80c
David Banks <dave@hoglet.com>
2019-06-02 14:07:12 +0100

    Add missing build.sh file

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SRC=dlair
+
+
+for mode in 0 2 4
+do
+    PROG=PLAYER${mode}
+
+    echo Assembling $PROG
+    ca65 -l ${PROG}.lst -DMODE=${mode} -o ${SRC}.o ${SRC}.asm
+    echo Linking ${PROG}
+    ld65 ${SRC}.o -C atom.cfg -o "${PROG}"
+    echo Finished, created ${PROG}
+    md5sum ${PROG}
+
+done
+
+rm -f ${SRC}.o
