62bf3f96421a3fa68d251015280f0d50d33cd793
David Banks <dave@hoglet.com>
2019-06-03 12:51:44 +0100

    Also detect 8MHz systems

--- via.asm
+++ via.asm
@@ -9,24 +9,26 @@
          SIDBase       = $bdc0
          SIDMusicLoad  = $1000
 
-         ; SIDIrqTime    = 10000
+         ; SIDIrqTime    = 20000
 
          ; Slow down by 10% so SID length matches Video length
          ; Why is this necessary???
-         SIDIrqTime    = 11050
+         SIDIrqTime    = 22100
 
 TimerLo:
-        .byte <(SIDIrqTime), <(SIDIrqTime * 2), <(SIDIrqTime * 4)
+        .byte <(SIDIrqTime), <(SIDIrqTime * 2), <(SIDIrqTime * 2), <(SIDIrqTime * 2)
 
 TimerHi:
-        .byte >(SIDIrqTime), >(SIDIrqTime * 2), >(SIDIrqTime * 4)
+        .byte >(SIDIrqTime), >(SIDIrqTime * 2), >(SIDIrqTime * 2), >(SIDIrqTime * 2)
+
+TimerMask:
+        .byte 0, 0, 1, 3
 
 SystemSpeed:
         .byte 0
 
 OldVec:
         .byte 0,0
-
 
 InitVIA:
 
@@ -53,20 +55,25 @@ InitVIA:
         ; at 1MHZ it will be 255 -  16666/256 = 190
         ; at 2MHZ it will be 255 -  33333/256 = 125
         ; at 4MHz it will be 255 -  66666/256 = -5 = 251
-        ldx #2
-        cmp #220
-        bcs VsyncTestDone
-        dex
-        cmp #150
-        bcc VsyncTestDone
+        ; at 8MHz it will be 255 - 133333/256 = -265 = 247
+        ldx #3
+        cmp #249              ; is it 4MHz
+        bcs  VsyncTestDex     ; yes, then branch, decrementing X to 2
+        cmp #240              ; is it 8MHz
+        bcs VsyncTestDone     ; yes, then branch, leaving X at 3
+        ldx #1
+        cmp #150              ; is it 2MHz
+        bcc VsyncTestDone     ; yes, then branch leaving X at 1
+VsyncTestDex:
         dex
 VsyncTestDone:
-        stx SystemSpeed       ; 0 = 1MHz, 1 = 2MHz, 2 = 4MHz
+        stx SystemSpeed       ; 0 = 1MHz, 1 = 2MHz, 2 = 4MHz, 3 = 8MHz
         lda TimerLo,X         ; 10ms timer interrupts
         sta ViaT1CounterL
         lda TimerHi,X
         sta ViaT1CounterH
-
+        lda TimerMask,X
+        sta ISRCounterMask
         lda ViaACR
         and #$7f
         ora #$40
@@ -93,17 +100,21 @@ ResetVIA:
         cli
         rts
 
-ISRFlag:
+ISRCounter:
+        .byte 0
+
+ISRCounterMask:
         .byte 0
 
 TimerISR:
         lda ViaT1CounterL
 
-
-        lda ISRFlag
-        eor #$80
-        sta ISRFlag
-        bpl TimerISRExit
+        lda ISRCounter
+        clc
+        adc #1
+        sta ISRCounter
+        and ISRCounterMask
+        bne TimerISRExit
 
         txa
         pha
